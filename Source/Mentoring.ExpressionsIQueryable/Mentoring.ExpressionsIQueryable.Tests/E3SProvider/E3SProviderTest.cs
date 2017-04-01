4e1d222fae87be9fefa5a9e65dfc2db9f881c20a
YauheniyaSmolnikava <evgy.luts@gmail.com>
2017-04-01 22:44:06 +0300

    added test

--- Source/Mentoring.ExpressionsIQueryable/Mentoring.ExpressionsIQueryable.Tests/E3SProvider/E3SProviderTest.cs
+++ Source/Mentoring.ExpressionsIQueryable/Mentoring.ExpressionsIQueryable.Tests/E3SProvider/E3SProviderTest.cs
@@ -11,35 +11,22 @@ namespace Mentoring.ExpressionsIQueryable.Tests.E3SProvider
     public class E3SProviderTest
     {
         [TestMethod]
-        public void WithoutProvider()
-        {
-            var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
-            var res = client.SearchFTS<EmployeeEntity>("workstation:(EPRUIZHW0249)", 0, 1);
-
-            foreach (var emp in res)
-            {
-                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
-            }
-        }
-
-        [TestMethod]
-        public void WithoutProviderNonGeneric()
-        {
-            var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
-            var res = client.SearchFTS(typeof(EmployeeEntity), "workstation:(EPRUIZHW0249)", 0, 10);
-
-            foreach (var emp in res.OfType<EmployeeEntity>())
-            {
-                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
-            }
-        }
-
-        [TestMethod]
-        public void WithProvider()
+        public void WithProviderAndContains()
         {
             var employees = new E3SLinqProvider.E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
 
             foreach (var emp in employees.Where(e => e.workstation == "EPRUIZHW0249" && e.nativename.Contains("Михаил")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+            }
+        }
+
+        [TestMethod]
+        public void WithProviderAndStartsWithEnsWith()
+        {
+            var employees = new E3SLinqProvider.E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => e.workstation.StartsWith("EPRUIZHW02") && e.nativename.EndsWith("Романов")))
             {
                 Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
             }
