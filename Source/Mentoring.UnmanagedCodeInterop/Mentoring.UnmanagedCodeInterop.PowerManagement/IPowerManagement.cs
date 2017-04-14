884b6856c8257dd78f6a387c7dd81f5c9c3655c1
Yauheniya Smolnikava <Yauheniya_Smolnikava@epam.com>
2017-04-14 14:28:14 +0300

    com properties

---
+++ Source/Mentoring.UnmanagedCodeInterop/Mentoring.UnmanagedCodeInterop.PowerManagement/IPowerManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentoring.UnmanagedCodeInterop.PowerManagement
+{
+    public interface IPowerManagement
+    {
+        string GetLastSleepTime();
+
+        string GetLastWakeTime();
+
+        string GetSystemBatteryState();
+
+        string GetSystemPowerInformation();
+
+        string ReserveHibernationFile();
+
+        void DeleteHibernationFile();
+
+        void ForceHibernate();
+    }
+}
---
+++ Source/Mentoring.UnmanagedCodeInterop/Mentoring.UnmanagedCodeInterop.PowerManagement/PowerManagement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentoring.UnmanagedCodeInterop.PowerManagement
+{
+    [ComVisible(true)]
+    [Guid("4C4FE3DE-7461-435B-80BA-08EF415D2923")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class PowerManagement : IPowerManagement
+    {
+        public string GetLastSleepTime()
+        {
+            return PowerManagementInterop.GetLastSleepTime();
+        }
+
+        public string GetLastWakeTime()
+        {
+            return PowerManagementInterop.GetLastWakeTime();
+        }
+
+        public string GetSystemBatteryState()
+        {
+            return PowerManagementInterop.GetSystemBatteryState();
+        }
+
+        public string GetSystemPowerInformation()
+        {
+            return PowerManagementInterop.GetSystemPowerInformation();
+        }
+
+        public string ReserveHibernationFile()
+        {
+            return PowerManagementInterop.ReserveHibernationFile();
+        }
+
+        public void DeleteHibernationFile()
+        {
+            PowerManagementInterop.DeleteHibernationFile();
+        }
+
+        public void ForceHibernate()
+        {
+            PowerManagementInterop.ForceHibernate();
+        }
+    }
+}
