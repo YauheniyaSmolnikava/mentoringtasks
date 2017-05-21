ca5156fd3d1a1cc2090f277159e4b68ce5eb086a
YauheniyaSmolnikava <evgy.luts@gmail.com>
2017-05-21 13:04:19 +0300

    debugging

---
+++ Source/Mentoring.Debugging/Mentoring.Debugging.KeyGen/KeyGenForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Mentoring.Debugging.KeyGen
+{
+    partial class KeyGenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.key = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(12, 33);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(104, 28);
+            this.btn_generate.TabIndex = 0;
+            this.btn_generate.Text = "Generate Key";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(122, 38);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(278, 20);
+            this.key.TabIndex = 1;
+            // 
+            // KeyGenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 100);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.btn_generate);
+            this.Name = "KeyGenForm";
+            this.Text = "KeyGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.TextBox key;
+    }
+}
+
---
+++ Source/Mentoring.Debugging/Mentoring.Debugging.KeyGen/KeyGenForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Mentoring.Debugging.KeyGen
+{
+    public partial class KeyGenForm : Form
+    {
+        public KeyGenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            var networkInterface = ((IEnumerable<NetworkInterface>)NetworkInterface.GetAllNetworkInterfaces()).FirstOrDefault<NetworkInterface>();
+            byte[] addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+            var dates = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            Func<byte, int, int> selector = new Func<byte, int, int>((A_0, A_1) => { return (int)(A_0 ^ dates[A_1]); });
+            int[] array = ((IEnumerable<byte>)addressBytes).Select<byte, int>(selector).Select<int, int>((Func<int, int>)(A_0_2 => { return A_0_2 * 10; })).ToArray();
+            key.Text = string.Join("-", array);
+        }
+    }
+}
