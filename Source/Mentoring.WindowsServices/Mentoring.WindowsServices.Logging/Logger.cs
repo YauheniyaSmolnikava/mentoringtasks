678c028c4abb1aa11c2db2b9d9904a484f2f2943
YauheniyaSmolnikava <evgy.luts@gmail.com>
2017-06-10 00:51:20 +0300

    fixed

---
+++ Source/Mentoring.WindowsServices/Mentoring.WindowsServices.Logging/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Castle.DynamicProxy;
+using Newtonsoft.Json;
+
+namespace Mentoring.WindowsServices.Logging
+{
+    public class Logger : IInterceptor
+    {
+        private string log = "log.txt";
+
+        public void Intercept(IInvocation invocation)
+        {
+            var curDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            var filePath = Path.Combine(curDir, log);
+
+            invocation.Proceed();
+
+            var logInfo = new LogInfo
+            {
+                InParams = string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()),
+                MethodName = invocation.Method.Name,
+                Time = DateTime.Now.ToLongTimeString(),
+                ReturnedValue = invocation.ReturnValue.ToString()
+            };
+
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(JsonConvert.SerializeObject(logInfo));
+            }
+        }
+    }
+}
---
+++ Source/Mentoring.WindowsServices/Mentoring.WindowsServices.Logging/LoggerPostSharpAspect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using PostSharp.Aspects;
+
+namespace Mentoring.WindowsServices.Logging
+{
+    [Serializable]
+    public class LoggerPostSharpAspect : OnMethodBoundaryAspect
+    {
+        private LogInfo logInfo;
+        private string log = "logPostSharp.txt";
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            logInfo = new LogInfo
+            {
+                MethodName = args.Method.Name,
+                InParams = string.Join(", ", args.Arguments.Select(a => (a ?? "").ToString()).ToArray()),
+                Time = DateTime.Now.ToLongTimeString()
+            };
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            var curDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            var filePath = Path.Combine(curDir, log);
+
+            logInfo.ReturnedValue = args.ReturnValue.ToString();
+
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(JsonConvert.SerializeObject(logInfo));
+            }
+        }
+    }
+}
